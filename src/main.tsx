b800f14ed3ccfc01df665fc60be9123b55826217
MariiaHula <mariyagulaya@gmail.com>
2024-02-02 15:01:25 +0200

    Update main.tsx
    added BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { store, persistor } from "./redux/store.ts";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  //need to add basename="https://monkey-plant.onrender.com/ in BrowserRouter"
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
 );
-
