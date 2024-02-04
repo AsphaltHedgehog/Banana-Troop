ba31393f53f32edc283cef9e4fca4dee71997dc2
Mykyta Kutelmakh <105374551+Reinghord@users.noreply.github.com>
2024-02-04 20:38:29 +0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,14 @@
 import Block from "../../components/block";
 import Box from "../../components/box/Box";
 import Stats from "../../components/stats/Stats";
-import QuizListItem from "../../shared/quizlistitem/QuizListItem";
 
 const Home = () => {
   return (
     <>
-            <QuizListItem />
       <Box>
         <Block />
       </Box>
       <Stats />
-
     </>
   );
 };
