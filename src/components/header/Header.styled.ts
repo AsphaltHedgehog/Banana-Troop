bc107948252496c6997814d55cf981de8161ded7
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-07 18:09:03 +0200

    added burger open icon svg

--- src/components/header/Header.styled.ts
+++ src/components/header/Header.styled.ts
@@ -3,14 +3,18 @@ import styled from "styled-components";
 export const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  /* width: 100vw; */
+  padding: 20px;
 `;
 
 export const StyledH2 = styled.h2`
-  /* margin-top: 28px; */
   font-weight: 700;
   font-size: 24px;
   line-height: 1;
   letter-spacing: -0.01em;
   color: #f4f4f4;
+`;
+
+export const SvgBurgerMenu = styled.svg`
+  width: 32px;
+  height: 32px;
 `;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
-import { HeaderWrapper, StyledH2 } from "./Header.styled";
+import { HeaderWrapper, StyledH2, SvgBurgerMenu } from "./Header.styled";
+import sprite from "../../images/icons/sprite.svg";
 
 const Header: FC = () => {
   return (
     <HeaderWrapper>
       <StyledH2>QuizMaster</StyledH2>
+      <SvgBurgerMenu>
+        <use xlinkHref={`${sprite}#icon-align-justify`}></use>
+      </SvgBurgerMenu>
     </HeaderWrapper>
   );
 };
