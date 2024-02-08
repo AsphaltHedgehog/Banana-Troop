29e14dfb6efa8f29e3202639bdb63dd77f86b7a7
MariiaHula <mariyagulaya@gmail.com>
2024-02-08 11:00:20 +0200

    fix reduxes updateOption questions quiz

---
+++ src/components/questionData/answers/Answers.styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const QuestionInputAnswerQuiz = styled.input``;
+export const QuestionInputAnswerTrueFalse = styled.input``;
+export const QuestionInputAnswerCheckbox = styled.input``;
