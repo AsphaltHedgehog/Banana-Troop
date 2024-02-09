69c1e0ca5f15edcfd2476cdcf1f3fda1e4f090d7
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-09 18:25:48 +0200

    added styles for burger menu modal

--- src/components/header/wholeComponent/Header.tsx
+++ src/components/header/wholeComponent/Header.tsx
@@ -3,11 +3,8 @@ import { HeaderWrapper, StyledH2 } from "./Header.styled";
 import BurgerMenu from "../burgerMenu/BurgerMenu";
 import { useNavigate } from "react-router";
 
-// import UserDropout from "../userDropout/userDropout";
-
 const Header: FC = () => {
   const navigate = useNavigate();
-  // const [endAnimation, setEndAnimation] = useState<boolean>(false);
 
   const handleGoHome = () => {
     navigate("/");
@@ -15,12 +12,8 @@ const Header: FC = () => {
 
   return (
     <HeaderWrapper>
-      {/* <UserDropout /> */}
       <StyledH2 onClick={handleGoHome}>QuizMaster</StyledH2>
-      <BurgerMenu
-      // endAnimation={endAnimation}
-      // setEndAnimation={setEndAnimation}
-      />
+      <BurgerMenu />
     </HeaderWrapper>
   );
 };
