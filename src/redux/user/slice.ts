443d428ac3d81db7455a6be0129d39d7bfd14c75
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-09 19:17:35 +0200

    added user slice
    made extrareducer for getUserThunk

--- src/redux/user/slice.ts
+++ src/redux/user/slice.ts
@@ -1,1 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getUserThunk } from "./operations";
 
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  favorite?: string[];
+}
+
+export interface UserState {
+  user: User;
+  error: string | null;
+  isLoading: boolean;
+}
+
+const initialState: UserState = {
+  user: {
+    _id: "",
+    name: "",
+    email: "",
+    favorite: [],
+  },
+  error: null,
+  isLoading: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserThunk.fulfilled, (state, { payload }) => {
+      state.user = payload as User;
+    });
+  },
+});
+export const userReducer = userSlice.reducer;
