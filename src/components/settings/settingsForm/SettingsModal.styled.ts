f955f41834db18fb806b19bc8e572667792b9516
Kreal11 <kirillshyrokov2@gmail.com>
2024-02-11 02:55:43 +0200

    added yup styles

--- src/components/settings/settingsForm/SettingsModal.styled.ts
+++ src/components/settings/settingsForm/SettingsModal.styled.ts
@@ -1,4 +1,10 @@
-import { styled } from "styled-components";
+import { styled, css } from "styled-components";
+
+interface SettingsInputProps {
+  $error?: string;
+  $nameValue?: string;
+  $isValidNameValue: boolean;
+}
 
 export const SettingsPhotoWrapper = styled.div`
   svg {
@@ -15,23 +21,8 @@ export const SettingsForm = styled.form`
   gap: 24px;
   position: relative;
 
-  input {
-    color: white;
-    width: 295px;
-    background-color: transparent;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.14;
-    letter-spacing: -0.01em;
-    text-align: left;
-    padding: 14px 18px;
-    border-radius: 30px;
-    border: 1px solid #f4f4f44d;
-  }
-
   p {
     position: absolute;
-    color: #e74a3b;
     font-size: 10px;
     font-weight: 400;
     line-height: 1.2;
@@ -40,14 +31,66 @@ export const SettingsForm = styled.form`
   }
 `;
 
+export const SettingsInput = styled.input<SettingsInputProps>`
+  color: white;
+  width: 295px;
+  background-color: transparent;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.14;
+  letter-spacing: -0.01em;
+  text-align: left;
+  padding: 14px 18px;
+  border-radius: 30px;
+  border: 1px solid ${({ $error }) => ($error ? "#e74a3b" : "#f4f4f44d")};
+  /* outline: none; */
+
+  &:focus {
+    border: 1px solid white;
+    ${({ $error, $nameValue, $isValidNameValue }) =>
+      !$error &&
+      $nameValue &&
+      $isValidNameValue &&
+      css`
+        border: 1px solid #097b45;
+      `}
+    ${({ $error, $isValidNameValue, $nameValue }) =>
+      ($error || $nameValue) &&
+      !$isValidNameValue &&
+      css`
+        border: 1px solid #e74a3b;
+      `}
+  }
+
+  &:focus:not(.focus-visible) {
+    outline: none;
+  }
+`;
+
 export const StyledNameError = styled.p`
+  color: #e74a3b;
   top: 50px;
 `;
 
 export const StyledEmailError = styled.p`
+  color: #e74a3b;
   top: 120px;
 `;
 
 export const StyledPasswordError = styled.p`
+  color: #e74a3b;
+  top: 188px;
+`;
+
+export const StyledNameValid = styled.p`
+  color: #097b45;
+  top: 50px;
+`;
+export const StyledEmailValid = styled.p`
+  color: #097b45;
+  top: 120px;
+`;
+export const StyledPasswordValid = styled.p`
+  color: #097b45;
   top: 188px;
 `;
--- src/components/settings/settingsForm/SettingsModal.tsx
+++ src/components/settings/settingsForm/SettingsModal.tsx
@@ -1,31 +1,48 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import sprite from "../../../images/icons/sprite.svg";
 import {
   SettingsForm,
+  SettingsInput,
   SettingsPhotoWrapper,
   StyledEmailError,
-  StyledErrorP,
+  StyledEmailValid,
   StyledNameError,
+  StyledNameValid,
   StyledPasswordError,
+  StyledPasswordValid,
 } from "./SettingsModal.styled";
-import { useAppDispatch } from "../../../redux/hooks";
+// import { useAppDispatch } from "../../../redux/hooks";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaSettingsInput } from "../../../helpers/schemas";
 
 const SettingsModal: FC = () => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
+  const [isValidNameValue, setIsValidNameValue] = useState(false);
 
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
+    watch,
   } = useForm({
     resolver: yupResolver(schemaSettingsInput),
   });
 
-  const submit = ({ name, email, password }) => {
+  const nameValue = watch("name");
+  const emailValue = watch("email");
+  const passwordValue = watch("password");
+
+  useEffect(() => {
+    if (nameValue?.length >= 1 && nameValue?.length <= 32) {
+      setIsValidNameValue(true);
+    } else {
+      setIsValidNameValue(false);
+    }
+  }, [nameValue?.length]);
+
+  const submit = () => {
     reset();
   };
 
@@ -38,17 +55,46 @@ const SettingsModal: FC = () => {
         </svg>
       </SettingsPhotoWrapper>
       <SettingsForm onSubmit={handleSubmit(submit)}>
-        <input type="text" placeholder="Name" {...register("name")} />
-        {errors?.name && (
-          <StyledNameError>{errors.name.message}</StyledNameError>
+        <SettingsInput
+          type="text"
+          placeholder="Name"
+          {...register("name")}
+          $error={errors.name?.message}
+          $nameValue={nameValue}
+          $isValidNameValue={isValidNameValue}
+        />
+        {(errors?.name || !isValidNameValue) && (
+          <StyledNameError>
+            {errors.name?.message ||
+              (nameValue && "The name length must be from 8 to 32 characters")}
+          </StyledNameError>
         )}
-        <input type="text" placeholder="Email" {...register("email")} />
+        {!errors.name && nameValue && isValidNameValue && (
+          <StyledNameValid>Valid name</StyledNameValid>
+        )}
+        <SettingsInput
+          type="text"
+          placeholder="Email"
+          {...register("email")}
+          $error={errors.email?.message}
+        />
         {errors?.email && (
           <StyledEmailError>{errors.email.message}</StyledEmailError>
         )}
-        <input type="text" placeholder="Password" {...register("password")} />
+        {!errors.email && emailValue && (
+          <StyledEmailValid>Valid email</StyledEmailValid>
+        )}
+        <SettingsInput
+          type="text"
+          placeholder="Password"
+          {...register("password")}
+          $error={errors.password?.message}
+        />
         {errors?.password && (
           <StyledPasswordError>{errors.password.message}</StyledPasswordError>
+        )}
+        {!errors.password && passwordValue && (
+          <StyledPasswordValid>Valid password</StyledPasswordValid>
         )}
         <button>Save</button>
       </SettingsForm>
