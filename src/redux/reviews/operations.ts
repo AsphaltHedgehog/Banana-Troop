e8f7c14ac6ce7ef791a16a4f929a05ca259d5518
Olena <helensmart2323@gmail.com>
2024-02-12 23:58:45 +0100

    reviewsPostThunk

--- src/redux/reviews/operations.ts
+++ src/redux/reviews/operations.ts
@@ -9,11 +9,20 @@ interface Review {
   _id: string;
   userName: string;
   avatarUrl: string;
+  rating: string;
   review: string;
   createdAt: string;
   updatedAt: string;
 }
-
+interface ReviewsPost {
+  _id: string;
+  userName: string;
+  avatarUrl: string;
+  review: string;
+  createdAt: string;
+  updatedAt: string;
+  rating: string;
+}
 // interface Reviews {
 //   reviews: Review[];
 // }
@@ -39,8 +48,24 @@ export const reviewsThunk = createAsyncThunk<Review[], ReviewsThunkParams>(
           },
         }
       );
-
       return data.data as Review[];
+    } catch (error) {
+      if (error instanceof Error && typeof error.message === "string") {
+        return thunkApi.rejectWithValue(error.message);
+      } else {
+        return thunkApi.rejectWithValue("Произошла неизвестная ошибка");
+      }
+    }
+  }
+);
+export const reviewsPostThunk = createAsyncThunk<ReviewsPost>(
+  "reviews",
+  async (_: void, thunkApi) => {
+    try {
+      const { data }: AxiosResponse<ReviewsPost> = await quizApi.post(
+        "/reviews/:id/addReview"
+      );
+      return data;
     } catch (error) {
       if (error instanceof Error && typeof error.message === "string") {
         return thunkApi.rejectWithValue(error.message);
