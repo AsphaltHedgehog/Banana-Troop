42eba6a74ab39f4cb48076b39f4e0381137f1461
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-02-13 01:53:48 +0200

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
           {/* <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="logout" element={<Logout />} /> */}
-          <Route path="quizMachen" element={<QuizMachen />} />
+          <Route path="quizMachen/:id" element={<QuizMachen />} />
           <Route path="restorePassword" element={<RestorePassword />} />
           <Route path="newPassword/:resetToken" element={<NewPassword />} />
           <Route path="forAdults" element={<ForAdults />} />
