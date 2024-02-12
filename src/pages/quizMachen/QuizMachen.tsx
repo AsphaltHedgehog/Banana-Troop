85223ba7eef1b08be2d2c02337463052d9ddef24
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-02-13 01:54:18 +0200

    started Machen  page

--- src/pages/quizMachen/QuizMachen.tsx
+++ src/pages/quizMachen/QuizMachen.tsx
@@ -1,27 +1,137 @@
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+// redux
+import { useAppDispatch } from "../../redux/hooks";
+import { getQuizByIdThunk } from "../../redux/updateOptions/operations";
+import { useSelector } from "react-redux";
+import { selectGetQuiz, selectIsError, selectIsLoading } from "../../redux/quizMachen/selectors";
+import { selectGetUser } from "../../redux/user/selectors";
+import { Quiz } from "../../redux/quizMachen/slice";
 
 
-const QuizMachen = (quizId) => {
+const QuizMachen = () => {
+  const dispatch = useAppDispatch();
+  const { id } = useParams<{ id: string }>();
+  
+  // states
   const [Name, SetName] = useState('');
+  const [Index, SetIndex] = useState(-1)
 
-  const  SubmitHandler = (e) => {
-    e.preventDefault()
-  }
+  // quiz selectors
+  const quiz = useSelector(selectGetQuiz);
+  const isLoading = useSelector(selectIsLoading);
+  // const isError = useSelector(selectIsError);
+  console.log(quiz);
+
+  // destructurization
+  const {
+    // background,
+    theme,
+    // _id,
+    questions
+  } = quiz
+
+  // auth selectors
+  const user = useSelector(selectGetUser);
+
+  useEffect(() => {
+    if (!id) {
+
+
+      return
+    }
+
+    dispatch(getQuizByIdThunk(id));
+
+  }, [dispatch, id]);
+
+  // functional functions
+
+  const submitHandler = (e?: FormEvent<HTMLFormElement>) => {
+    if (e) {
+      e.preventDefault()
+    }
+    
+    SetIndex(Index + 1)
+  };
+  
+
+  // rendering functions
+
+
+  const answersType = () => {
+    if (!questions || !questions[Index]) {
+      throw new Error;
+    }
+
+    const answersArray = questions[Index].answers.map((el, index, array) => {
+      console.log("Ответы", index, el);
+      
+    })
+
+    return (
+      <div>
+
+      </div>
+    )
+  };
+
+
+  const questionInterface = () => {
+    if (!questions || !questions[Index]) {
+      throw new Error;
+    }
+    console.log(quiz);
+    
+
+    return (
+      <div>
+        {questions[Index].imageUrl ?
+          <>
+            <img src={`https://res.cloudinary.com/dddrrdx7a/image/upload/v1707757640/${questions[Index].imageUrl}`}></img>
+            <div>
+              <p>Time: {questions[Index].time}</p>
+              <p>{questions[Index].descr}</p>
+            </div>
+            <div>
+              {answersType()}
+            </div>
+          </>
+          : <>aaa</>
+
+          
+        }
+        
+      </div>
+    )
+  };
+
+  // component return
 
   return (
+    // add error toast
     <>
-      <h1>Log in to take the quiz</h1>
-      <p></p>
-      <form onSubmit={SubmitHandler}>
-        <input type="text"
-          value={Name}
-          onChange={e => SetName(e.target.value)}
-          placeholder="Name"
-        ></input>
-        <button type="submit" disabled={!Name}>Start</button>
-      </form>
+      {Index >= 0 ? questionInterface():
+        user._id ? submitHandler :
+          <>
+            {isLoading ? 'Place for loader' :
+              <>
+                <h1>Log in to take the quiz</h1>
+                <p>{theme}</p>
+                <form onSubmit={submitHandler}>
+                  <input type="text"
+                    value={Name}
+                    onChange={e => SetName(e.target.value)}
+                    placeholder="Name"
+                  ></input>
+                  <button type="submit" disabled={!Name}>Start</button>
+                </form>
+              </>
+            }
+          </>
+      }
     </>
-    
   )
 };
 
